1fce4467aaf6b91c53056a35d8acd5c8414f705f
glopezhe97 <gerlohe97@gmail.com>
2025-12-21 13:03:12 +0100

    RTM-back create playlists fix

--- src/playlists/controllers/playlists/playlists.controller.ts
+++ src/playlists/controllers/playlists/playlists.controller.ts
@@ -1,7 +1,11 @@
-import { Controller, Get, Param, Req } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Req } from '@nestjs/common';
 import { PlaylistsService } from '../../services/playlists/playlists.service';
 import { PlaylistDto } from '../../dto/playlist.dto/playlist.dto';
 import { AuthenticatedRequestDto } from 'src/shared/dto/authenticated-request.dto/authenticated-request.dto';
+
+export class CreatePlaylistDto {
+  name: string;
+}
 
 @Controller('playlists')
 export class PlaylistsController {
@@ -40,6 +44,32 @@ export class PlaylistsController {
     const playlist = await this.playlistsService.getPlaylistById(
       id,
       req.user?.id,
+    );
+    return this.playlistsService.toPlaylistDto(playlist);
+  }
+
+  @Post('create')
+  async createPlaylist(
+    @Body() playlistData: CreatePlaylistDto,
+    @Req() req: AuthenticatedRequestDto,
+  ): Promise<PlaylistDto> {
+    const playlist = await this.playlistsService.createPlaylist(
+      { name: playlistData.name },
+      req.user,
+    );
+    return this.playlistsService.toPlaylistDto(playlist);
+  }
+
+  @Post(':id/tracks')
+  async addTrackToPlaylist(
+    @Param('id') id: number,
+    @Body('trackExternalId') trackExternalId: string,
+    @Req() req: AuthenticatedRequestDto,
+  ): Promise<PlaylistDto> {
+    const playlist = await this.playlistsService.addTrackToPlaylist(
+      id,
+      trackExternalId,
+      req.user.id,
     );
     return this.playlistsService.toPlaylistDto(playlist);
   }
